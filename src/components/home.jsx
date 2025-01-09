8d9b01549ff05afb3cfd9e3be5c4c46951240540
Claire Rhodes <claire.rhodes13@gmail.com>
2025-01-09 14:11:15 -0800

    init commit

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import Navbar from '.nav.jsx';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default Home;
---
+++ src/components/nav.jsx
@@ -0,0 +1,19 @@
+
+const Navbar = ({handleSection}) => {
+    return (
+        <nav>
+            <button onClick={handleSection} value="Home">Home</button>
+            <button onClick={handleSection} value="">My Profile</button>
+            <button onClick={handleSection} value="">New Post</button>
+            <button onClick={handleSection} value="">Log Out</button>
+        </nav>
+    );
+};
+
+export default Navbar;
+
+// view own User
+// view other users
+// new post
+// view post
+// update/delete post
